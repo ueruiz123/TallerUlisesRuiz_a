2f95e4276705da3364c405424be17524e987275f
ueruiz123 <135989947+ueruiz123@users.noreply.github.com>
2023-06-15 09:41:00 -0500

    Add files via upload

---
+++ Programa.py
@@ -0,0 +1,7 @@
+from matematica import sumar, restar, multiplicar, dividir
+from algebra import distancia
+print(sumar(3,4))
+print(restar(4,2))
+print(multiplicar(3,4))
+print(dividir(10,2))
+print(distancia(1,2,3,4))
---
+++ algebra.py
@@ -0,0 +1,3 @@
+from math import sqrt
+def distancia(x1,y1,x2,y2):
+    return sqrt((x2-x1)**2+(y2-y1)**2)
